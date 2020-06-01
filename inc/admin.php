54937d627a3e5d3145b76525266aadfc5e516d90
Ben Marshall <bmarshall511@gmail.com>
2020-06-01 10:17:09 -0500

    Updates

--- inc/admin.php
+++ inc/admin.php
@@ -25,3 +25,57 @@ function hello_elementor_child_admin_menu() {
   }
 }
 add_action('admin_menu', 'hello_elementor_child_admin_menu');
+
+/**
+ * Update the default login logo with the site logo if available.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_login() {
+  $logo = get_theme_mod( 'custom_logo' );
+
+  if ( ! $logo ) { return; }
+
+  $image = wp_get_attachment_image_src( $logo, 'full' );
+  ?>
+  <style type="text/css">
+  #login h1 a, .login h1 a {
+    background-image: url('<?php echo $image[0]; ?>');
+    background-size: contain;
+    height: 25px;
+    width: 320px;
+    background-repeat: no-repeat;
+  }
+  </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'hello_elementor_child_login' );
+
+/**
+ * Filter the login logo URL.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_logo_url() {
+  return home_url();
+}
+add_filter( 'login_headerurl', 'hello_elementor_child_logo_url' );
+
+/**
+ * Filter the login logo title.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_logo_url_title() {
+  return get_bloginfo( 'name' );
+}
+add_filter( 'login_headertitle', 'hello_elementor_child_logo_url_title' );
+
+/**
+ * Enqueue admin & login scripts.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_login_scripts() {
+  wp_enqueue_style( 'hello-elementor-child-login', get_stylesheet_directory_uri() . '/assets/css/add-ons/wordpress/login.css', [], wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'login_enqueue_scripts', 'hello_elementor_child_login_scripts' );
