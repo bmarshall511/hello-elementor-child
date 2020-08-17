de89fce432b266e79061c6830a7021763e012184
Ben Marshall <bmarshall511@gmail.com>
2020-08-17 10:43:06 -0500

    updates

--- inc/admin.php
+++ inc/admin.php
@@ -13,18 +13,222 @@
  * @since 1.0.0
  */
 function hello_elementor_child_admin_menu() {
-  global $submenu;
+	global $submenu;
 
-  // Add the Sass Docs link if available
-  if ( is_dir( HELLO_ELEMENTOR_CHILD_DIRECTORY . '/assets/sassdoc' ) ) {
-    $submenu['themes.php'][] = [
-      __( 'Sass Docs', 'hello-elementor-child' ),
-      'edit_themes',
-      get_stylesheet_directory_uri() . '/assets/sassdoc/'
-    ];
-  }
+	add_submenu_page(
+		'themes.php',
+		__( 'Theme Settings', 'hello-elementor-child' ),
+		__( 'Theme Settings', 'hello-elementor-child' ),
+		'manage_options',
+		'hello-elementor-child-settings',
+		'hello_elementor_child_settings_page'
+	);
+
+	// Add the Sass Docs link if available.
+	if ( is_dir( HELLO_ELEMENTOR_CHILD_DIRECTORY . '/assets/sassdoc' ) ) {
+		$submenu['themes.php'][] = array(
+			__( 'Sass Docs', 'hello-elementor-child' ),
+			'edit_themes',
+			get_stylesheet_directory_uri() . '/assets/sassdoc/',
+		);
+	}
 }
-add_action('admin_menu', 'hello_elementor_child_admin_menu');
+add_action( 'admin_menu', 'hello_elementor_child_admin_menu' );
+
+/**
+ * The theme settings page.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_settings_page() {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+	?>
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<form action="options.php" method="post">
+		<?php
+		settings_fields( 'hello-elementor-child' );
+		do_settings_sections( 'hello-elementor-child' );
+		submit_button( 'Save Settings' );
+		?>
+		</form>
+	</div>
+	<?php
+}
+
+/**
+ * Validates the submitted theme options.
+ *
+ * @since 1.0.0
+ *
+ * @param array $input The submitted fields.
+ * @return array The validated submitted fields.
+ */
+function hello_elementor_child_validate_options( $input ) {
+	return $input;
+}
+
+/**
+ * Registers the theme settings.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_admin_init() {
+	register_setting( 'hello_elementor_child', 'hello_elementor_childd', 'hello_elementor_child_validate_options' );
+
+	add_settings_section( 'hello_elementor_child_general_settings', __( 'General Settings', 'hello-elementor-child' ), 'hello_elementor_child_general_settings_cb', 'hello-elementor-child' );
+
+	// Google Analytics tracking ID.
+	add_settings_field(
+		'ga_tracking_id',
+		__( 'Google Analytics Tracking ID', 'hello-elementor-child' ),
+		'hello_elementor_child_field_cb',
+		'hello-elementor-child',
+		'hello_elementor_child_general_settings',
+		array(
+			'label_for'   => 'ga_tracking_id',
+			'type'        => 'text',
+			'placeholder' => __( 'e.g. UA-XXXXXXXX-X', 'hello-elementor-child' ),
+			'class'       => 'regular-text',
+			'desc'        => 'Enter the sites\' Google Analytics tracking ID.',
+		)
+	);
+
+	// Google Font URL.
+	add_settings_field(
+		'google_font_url',
+		__( 'Google Fonts URL', 'hello-elementor-child' ),
+		'hello_elementor_child_field_cb',
+		'hello-elementor-child',
+		'hello_elementor_child_general_settings',
+		array(
+			'label_for'   => 'google_font_url',
+			'type'        => 'url',
+			'placeholder' => __( 'e.g. Copy & paste the Google Fonts URL.', 'hello-elementor-child' ),
+			'class'       => 'regular-text',
+			'desc'        => 'The Google Fonts URL.',
+		)
+	);
+}
+add_action( 'admin_init', 'hello_elementor_child_admin_init' );
+
+/**
+ * General settings section output.
+ *
+ * @since 1.0.0
+ */
+function hello_elementor_child_general_settings_cb() {
+	esc_html_e( 'Configure the theme\'s general settings below.', 'hello-elementor-child' );
+}
+
+/**
+ * Outputs a theme setting field.
+ *
+ * @since 1.0.0
+ *
+ * @param array $args The field arguments.
+ */
+function hello_elementor_child_field_cb( $args ) {
+	$options = get_option( 'hello_elementor_child' );
+
+	switch ( $args['type'] ) {
+		case 'url':
+		case 'text':
+		case 'password':
+		case 'number':
+		case 'email':
+			?>
+			<input
+				class="<?php echo esc_attr( $args['class'] ); ?>"
+				type="<?php echo esc_attr( $args['type'] ); ?>"
+				<?php if ( ! empty( $options[ $args['label_for'] ] ) ): ?>
+					value="<?php echo esc_attr( $options[ $args['label_for'] ] ); ?>"
+				<?php endif; ?>
+				<?php if ( ! empty( $args['placeholder'] ) ): ?>
+					placeholder="<?php echo esc_attr( $args['placeholder'] ); ?>"
+				<?php endif; ?>
+				id="<?php echo esc_attr( $args['label_for'] ); ?>"
+				name="hello_elementor_child[<?php echo esc_attr( $args['label_for'] ); ?>]"
+			>
+			<?php if ( ! empty( $args['suffix'] ) ) : ?>
+				<?php echo ' ' . wp_kses( $args['suffix'], array() ); ?>
+			<?php endif; ?>
+			<p class="description"><?php echo wp_kses( $args['desc'], array() ); ?></p>
+			<?php
+			break;
+		case 'textarea':
+			?>
+			<textarea
+				rows="10"
+				class="<?php echo esc_attr( $args['class'] ); ?>"
+				id="<?php echo esc_attr( $args['label_for'] ); ?>"
+				name="hello_elementor_child[<?php echo esc_attr( $args['label_for'] ); ?>]"
+			>
+				<?php if ( ! empty( $options[ $args['label_for'] ] ) ) : ?>
+					<?php echo esc_attr( $options[ $args['label_for'] ] ); ?>
+				<?php endif; ?>
+			</textarea>
+			<p class="description"><?php echo wp_kses( $args['desc'], array() ); ?></p>
+			<?php
+			break;
+		case 'select':
+			?>
+			<select name="hello_elementor_child[<?php echo esc_attr( $args['label_for'] ); ?>]" id="<?php echo esc_attr( $args['label_for'] ); ?>">
+				<?php foreach ( $args['options'] as $key => $label ) : ?>
+					<option value="<?php echo $key; ?>"<?php if ( $key === $options[ $args['label_for'] ] ) : ?>
+						selected="selected"
+					<?php endif; ?>><?php echo esc_html( $label ); ?></option>
+				<?php endforeach; ?>
+			</select>
+			<p class="description"><?php echo wp_kses( $args['desc'], array() ); ?></p>
+			<?php
+			break;
+		case 'checkbox':
+			?>
+			<?php foreach ( $args['options'] as $key => $label ): ?>
+				<label for="<?php echo esc_attr( $args['label_for'] . $key ); ?>">
+					<input
+						type="checkbox"
+						<?php if ( ! empty( $args['class'] ) ) : ?>
+							class="<?php echo esc_attr( $args['class'] ); ?>"
+						<?php endif; ?>
+						id="<?php echo esc_attr( $args['label_for'] . $key ); ?>"
+						name="hello_elementor_child[<?php echo esc_attr( $args['label_for'] ); ?>]<?php if ( $args['multi'] ): ?>[<?php echo esc_attr( $key ); ?>]<?php endif; ?>"
+						value="<?php echo esc_attr( $key ); ?>"
+						<?php if ( $args['multi'] && $key === $options[ $args['label_for'] ][ $key ] || ! $args['multi'] && $key === $options[ $args['label_for'] ] ): ?>
+							checked="checked"
+						<?php endif; ?>
+					/> <?php echo wp_kses( $label, array() ); ?>
+				</label>
+			<?php endforeach; ?>
+			<p class="description"><?php echo wp_kses( $args['desc'], array() ); ?></p>
+			<?php
+			break;
+		case 'radio':
+			?>
+			<?php foreach ( $args['options'] as $key => $label ): ?>
+				<label for="<?php echo esc_attr( $args['label_for'] . $key ); ?>">
+					<input
+						type="radio"
+						<?php if ( ! empty( $args['class'] ) ) : ?>
+							class="<?php echo esc_attr( $args['class'] ); ?>"
+						<?php endif; ?>
+						id="<?php echo esc_attr( $args['label_for'] . $key ); ?>"
+						name="hello_elementor_child[<?php echo esc_attr( $args['label_for'] ); ?>]" value="<?php echo esc_attr( $key ); ?>"
+						<?php if ( $key === $options[ $args['label_for'] ] ) : ?>
+							checked="checked"
+						<?php endif; ?>
+					/> <?php echo wp_kses( $label, array() ); ?>
+				</label><br />
+			<?php endforeach; ?>
+			<p class="description"><?php echo wp_kses( $args['desc'], array() ); ?></p>
+			<?php
+			break;
+	}
+}
+
 
 /**
  * Update the default login logo with the site logo if available.
--- inc/scripts.php
+++ inc/scripts.php
@@ -15,8 +15,8 @@ use MatthiasMullie\Minify;
  * @since 1.0.0
  */
 function hello_elementor_child_scripts() {
-  // Non-critial CSS, loaded on every page.
-  wp_enqueue_style( 'hello-elementor-child-non-critical', get_stylesheet_directory_uri() . '/assets/css/non-critical.css', [], wp_get_theme()->get( 'Version' ) );
+	// Non-critial CSS, loaded on every page.
+	wp_enqueue_style( 'hello-elementor-child-non-critical', get_stylesheet_directory_uri() . '/assets/css/non-critical.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts', PHP_INT_MAX );
 
@@ -24,17 +24,75 @@ add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts', PHP_INT_MAX )
  * Inline critical (above page fold) CSS.
  *
  * @since 1.0.0
- *
- * @return string HTML style tag & styles output for the critical CSS.
  */
 function hello_elementor_child_critical_path_css() {
-  $minifier = new Minify\CSS( get_stylesheet_directory() . '/assets/css/critical.css' );
-  ob_start();
-  ?>
-  <style>
-    <?php echo $minifier->minify(); ?>
-  </style>
-  <?php
-  echo ob_get_clean();
+	$minifier = new Minify\CSS( get_stylesheet_directory() . '/assets/css/critical.css' );
+	ob_start();
+	?>
+	<style>
+		<?php echo $minifier->minify(); ?>
+	</style>
+	<?php
+	echo ob_get_clean();
 }
 add_action( 'wp_head', 'hello_elementor_child_critical_path_css' );
+
+/**
+ * Preconnect & preload resources.
+ *
+ * @since 1.0.0
+ */
+add_action(
+	'wp_head',
+	function() {
+		$options = get_option( 'hello_elementor_child' );
+		if ( ! empty( $options['ga_tracking_id'] ) ) :
+			?>
+			<link href="https://www.google-analytics.com" rel="preconnect" crossorigin />
+		<?php endif; ?>
+		<?php if ( ! did_action( 'elementor/loaded' ) ): ?>
+			<link rel="preload" href="https://www.benmarshall.me/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-solid-900.woff2" as="font" crossorigin />
+		<?php endif; ?>
+		<?php if ( ! empty( $options['google_font_url'] ) ) : ?>
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link rel="preload" as="style" href="<?php echo esc_url( $options['google_font_url'] ); ?>" />
+			<link rel="stylesheet" href="<?php echo esc_url( $options['google_font_url'] ); ?>" media="print" onload="this.media='all'" />
+			<noscript>
+				<link rel="stylesheet" href="<?php echo esc_url( $options['google_font_url'] ); ?>" />
+			</noscript>
+		<?php
+		endif;
+	}
+);
+
+/**
+ * Add the GA tracking code if set.
+ *
+ * @since 1.0.0
+ */
+add_action(
+	'wp_footer',
+	function() {
+		if ( current_user_can( 'administrator' ) ) {
+			return;
+		}
+
+		$options = get_option( 'hello_elementor_child' );
+		if ( ! empty( $options['ga_tracking_id'] ) ) :
+			?>
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr( $options['ga_tracking_id'] ); ?>"></script>
+			<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', '<?php echo esc_attr( $options['ga_tracking_id'] ); ?>');
+			<?php if ( is_user_logged_in() ) : ?>
+				gtag('set', {'user_id': '<?php echo intval( get_current_user_id() ); ?>'}); // Set the user ID using signed-in user_id.
+			<?php endif; ?>
+			</script>
+			<?php
+		endif;
+	}
+);
