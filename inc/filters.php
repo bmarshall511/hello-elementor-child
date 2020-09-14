13fdf456d2af724df1216020644e59a7fe9a63d4
Ben Marshall <bmarshall511@gmail.com>
2020-09-14 10:25:01 -0500

    Updates

--- inc/filters.php
+++ inc/filters.php
@@ -6,6 +6,6 @@
  * @subpackage HelloElementorChild
  */
 
-add_filter( 'hello_elementor_enqueue_style', '__return_false' );
-add_filter( 'hello_elementor_enqueue_theme_style', '__return_false' );
+//add_filter( 'hello_elementor_enqueue_style', '__return_false' );
+//add_filter( 'hello_elementor_enqueue_theme_style', '__return_false' );
 add_filter( 'hello_elementor_page_title', '__return_false' );
