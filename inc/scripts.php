6366c1ccd51c70097ce0dea4298297a836ee98af
Ben Marshall <bmarshall511@gmail.com>
2020-09-14 09:35:03 -0500

    Updates

--- inc/scripts.php
+++ inc/scripts.php
@@ -12,8 +12,14 @@ use MatthiasMullie\Minify;
  * Register & enqueue theme CSS & JS files.
  */
 function hello_elementor_child_scripts() {
+	$options = get_option( 'hello_elementor_child' );
+
 	// Non-critial CSS, loaded on every page.
 	wp_enqueue_style( 'hello-elementor-child-non-critical', get_stylesheet_directory_uri() . '/assets/css/non-critical.css', array(), HELLO_ELEMENTOR_CHILD_VERSION );
+
+	if ( ! empty( $options['ga_tracking_id'] ) ) {
+		wp_enqueue_script( 'hello-elementor-child-ga', 'https://www.googletagmanager.com/gtag/js?id=' . $options['ga_tracking_id'], array(), HELLO_ELEMENTOR_CHILD_VERSION, true );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts', PHP_INT_MAX );
 
@@ -45,17 +51,19 @@ add_action(
 			?>
 			<link href="https://www.google-analytics.com" rel="preconnect" crossorigin />
 		<?php endif; ?>
-		<?php if ( ! did_action( 'elementor/loaded' ) ): ?>
+		<?php if ( ! did_action( 'elementor/loaded' ) ) : ?>
 			<link rel="preload" href="https://www.benmarshall.me/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-solid-900.woff2" as="font" crossorigin />
 		<?php endif; ?>
-		<?php if ( ! empty( $options['google_font_url'] ) ) : ?>
+		<?php
+		if ( ! empty( $options['google_font_url'] ) ) :
+			?>
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 			<link rel="preload" as="style" href="<?php echo esc_url( $options['google_font_url'] ); ?>" />
-			<link rel="stylesheet" href="<?php echo esc_url( $options['google_font_url'] ); ?>" media="print" onload="this.media='all'" />
+			<link rel="stylesheet" href="<?php echo esc_url( $options['google_font_url'] ); ?>" media="print" onload="this.media='all'" /> <?php // phpcs:ignore ?>
 			<noscript>
-				<link rel="stylesheet" href="<?php echo esc_url( $options['google_font_url'] ); ?>" />
+				<link rel="stylesheet" href="<?php echo esc_url( $options['google_font_url'] ); ?>" /> <?php // phpcs:ignore ?>
 			</noscript>
-		<?php
+			<?php
 		endif;
 	}
 );
@@ -75,8 +83,6 @@ add_action(
 		$options = get_option( 'hello_elementor_child' );
 		if ( ! empty( $options['ga_tracking_id'] ) ) :
 			?>
-			<!-- Global site tag (gtag.js) - Google Analytics -->
-			<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr( $options['ga_tracking_id'] ); ?>"></script>
 			<script>
 			window.dataLayer = window.dataLayer || [];
 			function gtag(){dataLayer.push(arguments);}
